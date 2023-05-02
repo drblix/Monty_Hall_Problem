bb5727005a77d8bd468122128db613355c8a5378
Ben <drblix75@gmail.com>
2023-05-02 14:37:48 -0400

    Initial commit

---
+++ MontyHallProblem/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MontyHallProblem
+{
+    partial class MontyWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AllYesToggle = new System.Windows.Forms.RadioButton();
+            this.AllNoToggle = new System.Windows.Forms.RadioButton();
+            this.RandomToggle = new System.Windows.Forms.RadioButton();
+            this.SwitchLabel = new System.Windows.Forms.Label();
+            this.IterationInput = new System.Windows.Forms.TextBox();
+            this.RunCountQ = new System.Windows.Forms.Label();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.PlayerPercent = new System.Windows.Forms.Label();
+            this.DetailedGames = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AllYesToggle
+            // 
+            this.AllYesToggle.AutoSize = true;
+            this.AllYesToggle.Location = new System.Drawing.Point(38, 102);
+            this.AllYesToggle.Name = "AllYesToggle";
+            this.AllYesToggle.Size = new System.Drawing.Size(59, 19);
+            this.AllYesToggle.TabIndex = 0;
+            this.AllYesToggle.TabStop = true;
+            this.AllYesToggle.Text = "All Yes";
+            this.AllYesToggle.UseVisualStyleBackColor = true;
+            this.AllYesToggle.CheckedChanged += new System.EventHandler(this.SwitchOption_Changed);
+            // 
+            // AllNoToggle
+            // 
+            this.AllNoToggle.AutoSize = true;
+            this.AllNoToggle.Location = new System.Drawing.Point(117, 102);
+            this.AllNoToggle.Name = "AllNoToggle";
+            this.AllNoToggle.Size = new System.Drawing.Size(58, 19);
+            this.AllNoToggle.TabIndex = 1;
+            this.AllNoToggle.TabStop = true;
+            this.AllNoToggle.Text = "All No";
+            this.AllNoToggle.UseVisualStyleBackColor = true;
+            this.AllNoToggle.CheckedChanged += new System.EventHandler(this.SwitchOption_Changed);
+            // 
+            // RandomToggle
+            // 
+            this.RandomToggle.AutoSize = true;
+            this.RandomToggle.Location = new System.Drawing.Point(197, 102);
+            this.RandomToggle.Name = "RandomToggle";
+            this.RandomToggle.Size = new System.Drawing.Size(70, 19);
+            this.RandomToggle.TabIndex = 2;
+            this.RandomToggle.TabStop = true;
+            this.RandomToggle.Text = "Random";
+            this.RandomToggle.UseVisualStyleBackColor = true;
+            this.RandomToggle.CheckedChanged += new System.EventHandler(this.SwitchOption_Changed);
+            // 
+            // SwitchLabel
+            // 
+            this.SwitchLabel.AutoSize = true;
+            this.SwitchLabel.Location = new System.Drawing.Point(33, 80);
+            this.SwitchLabel.Name = "SwitchLabel";
+            this.SwitchLabel.Size = new System.Drawing.Size(238, 15);
+            this.SwitchLabel.TabIndex = 3;
+            this.SwitchLabel.Text = "Should the contestant switch when offered?";
+            // 
+            // IterationInput
+            // 
+            this.IterationInput.Location = new System.Drawing.Point(96, 42);
+            this.IterationInput.Name = "IterationInput";
+            this.IterationInput.Size = new System.Drawing.Size(100, 23);
+            this.IterationInput.TabIndex = 4;
+            // 
+            // RunCountQ
+            // 
+            this.RunCountQ.AutoSize = true;
+            this.RunCountQ.Location = new System.Drawing.Point(30, 24);
+            this.RunCountQ.Name = "RunCountQ";
+            this.RunCountQ.Size = new System.Drawing.Size(241, 15);
+            this.RunCountQ.TabIndex = 5;
+            this.RunCountQ.Text = "How many times should the simulation run?";
+            // 
+            // RunButton
+            // 
+            this.RunButton.Location = new System.Drawing.Point(109, 129);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(75, 23);
+            this.RunButton.TabIndex = 6;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = true;
+            // 
+            // PlayerPercent
+            // 
+            this.PlayerPercent.AutoSize = true;
+            this.PlayerPercent.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PlayerPercent.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.PlayerPercent.Location = new System.Drawing.Point(34, 210);
+            this.PlayerPercent.Name = "PlayerPercent";
+            this.PlayerPercent.Size = new System.Drawing.Size(262, 23);
+            this.PlayerPercent.TabIndex = 8;
+            this.PlayerPercent.Text = "The player won 50% of all games";
+            this.PlayerPercent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DetailedGames
+            // 
+            this.DetailedGames.AutoSize = true;
+            this.DetailedGames.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DetailedGames.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DetailedGames.Location = new System.Drawing.Point(27, 179);
+            this.DetailedGames.Name = "DetailedGames";
+            this.DetailedGames.Size = new System.Drawing.Size(278, 22);
+            this.DetailedGames.TabIndex = 9;
+            this.DetailedGames.Text = "Out of {n_1} games, the player won {n_2}";
+            this.DetailedGames.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MontyWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 252);
+            this.Controls.Add(this.DetailedGames);
+            this.Controls.Add(this.PlayerPercent);
+            this.Controls.Add(this.RunButton);
+            this.Controls.Add(this.RunCountQ);
+            this.Controls.Add(this.IterationInput);
+            this.Controls.Add(this.SwitchLabel);
+            this.Controls.Add(this.RandomToggle);
+            this.Controls.Add(this.AllNoToggle);
+            this.Controls.Add(this.AllYesToggle);
+            this.Name = "MontyWindow";
+            this.Text = "Monty Hall Simulation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RadioButton AllYesToggle;
+        private RadioButton AllNoToggle;
+        private RadioButton RandomToggle;
+        private Label SwitchLabel;
+        private TextBox IterationInput;
+        private Label RunCountQ;
+        private Button RunButton;
+        private Label PlayerPercent;
+        private Label DetailedGames;
+    }
+}
---
+++ MontyHallProblem/Form1.cs
@@ -0,0 +1,103 @@
+
+namespace MontyHallProblem
+{
+    public partial class MontyWindow : Form
+    {
+        private enum Selection
+        {
+            Yes,
+            No,
+            Random
+        }
+
+        private Selection selection = Selection.Yes;
+
+        public MontyWindow()
+        {
+            InitializeComponent();
+            IterationInput.KeyPress += IterationInput_KeyPress;
+            RunButton.MouseClick += RunSimulation;
+        }
+
+        private void IterationInput_KeyPress(object? sender, KeyPressEventArgs e)
+        {
+            // Prevents non-digit characters from being entered, but allows backspace
+            e.Handled = !char.IsDigit(e.KeyChar) && !e.KeyChar.Equals('\b');
+        }
+
+        private void SwitchOption_Changed(object sender, EventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                selection = radioButton.Name switch
+                {
+                    "AllYesToggle" => Selection.Yes,
+                    "AllNoToggle" => Selection.No,
+                    _ => Selection.Random,
+                };
+            }
+        }
+
+        private void RunSimulation(object? sender, MouseEventArgs e)
+        {
+            // Parsing run amount to integer
+            int runAmount;
+            if (int.TryParse(IterationInput.Text, out int result))
+                runAmount = result;
+            else
+                return;
+
+            // Initializing
+            Random random = new();
+            int games = 0;
+            int wonGames = 0;
+
+            for (int i = 0; i < runAmount; i++)
+            {
+                // Creating doors list so host choice can be removed
+                List<int> doors = new() { 1, 2, 3 };
+
+                // Getting random choices for each
+                int prize = random.Next(0, doors.Count);
+                int playerChoice = random.Next(0, doors.Count);
+                int hostChoice = random.Next(0, doors.Count);
+
+                // Host's choice must NOT be the player's or prize
+                while (hostChoice == playerChoice || hostChoice == prize)
+                    hostChoice = random.Next(0, doors.Count);
+
+                // Removing host's choice from list
+                doors.Remove(doors[hostChoice]);
+
+                // Decrementing either variables if they were larger than the host's choice
+                // Accommodates the resizing of the list so there's no array out of bounds exceptions
+                if (playerChoice > hostChoice)
+                    playerChoice--;
+                if (prize > hostChoice)
+                    prize--;
+
+                // Will swap if yes or if random and rolled good number
+                if (selection == Selection.Yes || (selection == Selection.Random && random.Next(0, 2) == 0))
+                {
+                    // Swapping the index of player choice to other door
+                    if (playerChoice == 0)
+                        playerChoice = 1;
+                    else
+                        playerChoice = 0;
+                }
+
+                // Player wins if their choice is the prize door
+                if (playerChoice == prize)
+                    wonGames++;
+
+                // Increasing games played
+                games++;
+            }
+
+            // Formatting output
+            DetailedGames.Text = $"Out of {games} games, the player won {wonGames}";
+            double percent = wonGames / (double)games;
+            PlayerPercent.Text = $"The player won {Math.Round(percent, 3)}% of all games";
+        }
+    }
+}
